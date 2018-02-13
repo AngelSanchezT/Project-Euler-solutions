fa72ec36b8c3afdbb67378583084d97c42ea1ce6
'asanchez' <'ajsanchez@gmail.com'>
2018-02-12 19:19:09 -0500

    Correct error in push

--- java/euler/src/test/java/io/angelsanchezt/euler/AppTest.java
+++ java/euler/src/test/java/io/angelsanchezt/euler/AppTest.java
@@ -7,7 +7,7 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-<<<<<<< HEAD
+
 public class AppTest extends TestCase {
 	/**
 	 * Create the test case
@@ -32,34 +32,5 @@ public class AppTest extends TestCase {
 	public void testApp() {
 		assertTrue(true);
 	}
-=======
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
->>>>>>> branch 'master' of https://github.com/AngelSanchezT/Project-Euler-solutions.git
 }
