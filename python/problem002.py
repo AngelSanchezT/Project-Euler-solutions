2a3a40521c48eb6b677d01b60168225451bab768
Angel Sanchez <ajsanchez22@gmail.com>
2021-01-18 23:59:12 -0500

    Avance Problema 23

---
+++ python/problem002.py
@@ -0,0 +1,8 @@
+def solution(n):
+    
+    fibonaci
+    return fibonaci
+
+n = int(input("Escribe el limite de los numeros a evaluar:"))
+result = solution(n)
+print('El Fibonacci menor a  ' + str(n) + ' es: ' + str(result))
