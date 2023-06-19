10d3fd7a47d6b7ed637dbfb2da3c91f131f90bb2
AngelSanchezT <ajsanchez22@gmail.com>
2023-06-18 23:49:28 -0500

    Solution Problem 002

--- Javascript/problems/problem002.js
+++ Javascript/problems/problem002.js
@@ -1,6 +1,17 @@
 function problem002(target) {
-    return 0;
+    let sum = 0;
+    let a = 1;
+    let b = 1;
+    let c = a + b;
 
+    while(c < target) {
+        sum += c;
+        a = b + c;
+        b = c + a;
+        c = a + b;
+    }
+    
+    return sum;
 }
 
 module.exports = problem002;
--- Javascript/tests/problem002.test.js
+++ Javascript/tests/problem002.test.js
@@ -1,6 +1,7 @@
 const evenFibonacciNumbers = require('../problems/problem002');
 
 describe('Problem 2: Even Fibonacci Numbers', () => {
+
     test('Should return the of the even-valued terms the Fibonacci sequence.', () => {
         const target = 4000000;
         const valueExpect = 4613732;
