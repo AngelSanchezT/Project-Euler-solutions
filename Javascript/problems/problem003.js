b465030d1fa658bd416228946b7ded6f9ae37348
AngelSanchezT <ajsanchez22@gmail.com>
2023-06-19 00:28:21 -0500

    Solution Problem 003

--- Javascript/problems/problem003.js
+++ Javascript/problems/problem003.js
@@ -1,5 +1,33 @@
 function problem003(target) {
+    let lastFactor = 1;
+    if(target % 2 === 0) {
+        lastFactor = 2;
+        target = target / 2;
+        while(n % 2 === 0){
+            target = target / 2;
+        }
+    }
+    
+    let factor = 3;
+    let maxFactor  = Math.sqrt(target);
 
+    while(target > 1 && factor <= maxFactor) {
+        if(target % factor === 0) {
+            target = target / factor;
+            lastFactor = factor;
+            while(target % factor === 0) {
+                target = target / factor;
+            }
+            maxFactor = Math.sqrt(target);
+        }
+        factor = factor + 2;
+    }
+
+    if(target === 1) {
+        return lastFactor;
+    } else {
+        return target;
+    }
 }
 
 module.exports = problem003;
