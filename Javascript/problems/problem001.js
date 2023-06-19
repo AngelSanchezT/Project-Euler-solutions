8bfe843b54577b48aa84cb4e02f50cd1aed41a87
AngelSanchezT <ajsanchez22@gmail.com>
2023-06-18 23:23:07 -0500

    Solution Problem 001

--- Javascript/problems/problem001.js
+++ Javascript/problems/problem001.js
@@ -1,7 +1,9 @@
+
+
 function problem001(limit) {
     let sum = 0;
-    for(let i = 1; i <= limit; i ++) {
-        if(i % 3 === 0 || i % 5 === 0) {
+    for(let i = 1; i < limit; i ++) {
+        if((i % 3 === 0) || (i % 5 === 0)) {
             sum += i;
         }
     }
--- Javascript/tests/problem001.test.js
+++ Javascript/tests/problem001.test.js
@@ -1,8 +1,17 @@
 // import { problem001 } from '../problems/problem001';
 const multiplesOf3And5 = require('../problems/problem001');
-const limit = 10;
-const valueExpect = 23;
 
-test('Problem 1 - Sample Test Case', () => {
-    expect(multiplesOf3And5(limit)).toBe(valueExpect);
-});
+
+describe('Problem 1: Multiples of 3 and 5', () => {
+    test('should return the sum of all multiples of 3 and 5 below the given limit 10', () => {
+        const limit = 10;
+        const valueExpect = 23;
+        expect(multiplesOf3And5(limit)).toBe(valueExpect);
+    });
+    test('should return the sum of all multiples of 3 and 5 below the given limit 1000', () => {
+        const limit = 1000;
+        const valueExpect = 233168;
+        expect(multiplesOf3And5(limit)).toBe(valueExpect);
+    });
+})
+
